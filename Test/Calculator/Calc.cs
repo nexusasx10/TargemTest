5780ff26ca5e03b013ee6d58f4f070ad3cb16a1e
nexusasx10 <andrey.shellex.next@gmail.com>
2019-10-18 18:39:56 +0500

    Temp

--- Test/Calculator/Calc.cs
+++ Test/Calculator/Calc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using List;
 
 namespace Calculator
 {
@@ -7,6 +8,57 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            var list = new MyList<int>();
+            Console.WriteLine(list.Count);
+            list.Add(1);
+            Console.WriteLine(list.Count);
+            list.Add(2);
+            Console.WriteLine(list.Count);
+            list.Add(3);
+            Console.WriteLine(list.Count);
+            list.Clear();
+            Console.WriteLine(list.Count);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine();
+            Console.WriteLine(list.Contains(1));
+            Console.WriteLine(list.Contains(2));
+            Console.WriteLine(list.Contains(3));
+            Console.WriteLine(list.Contains(4));
+
+            Console.WriteLine();
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+            for (var i = 0; i < list.Count; i++)
+                Console.WriteLine(list[i]);
+
+            Console.WriteLine();
+            Console.WriteLine(list.IndexOf(2));
+
+            Console.WriteLine();
+            list.Insert(1, 4);
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+            list.RemoveAt(2);
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+            var a = new int[5];
+            list.CopyTo(a, 1);
+
+            Console.WriteLine();
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            foreach (var item in a)
+                Console.WriteLine(item);
+
             var tokens = new IToken[]
             {
                 new RegexToken("Space", @"\s+", 0),
@@ -21,6 +73,46 @@ namespace Calculator
             };
 
             var parser = new Parser(tokens, new List<string> { "Space" });
+
+            
+            try
+            {
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3+4")).Text == "7");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3 + 4")).Text == "7");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("12-4")).Text == "8");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("4-12")).Text == "-8");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("-23")).Text == "-23");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("- 23")).Text == "-23");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("2 * 2 + 2")).Text == "6");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("2 + 2 * 2")).Text == "6");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(2 + 2) * 2")).Text == "8");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("-(4 - 12)")).Text == "8");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(3*4)+(3*4)-(3*4)")).Text == "12");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(3*4)-(3*4)+(3*4)")).Text == "12");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("--3")).Text == "3");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("---3")).Text == "-3");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(2 + 1) * (2 * (3 - 5))")).Text == "-12");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("((((((((2+3))))))))")).Text == "5");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(2 + (2 + (2 + 2)))")).Text == "8");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(-2 + (2 + (2 + 2)))")).Text == "4");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3/4")).Text == "0.75");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("4*3/2")).Text == "6");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("10/2/5")).Text == "1");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3.5")).Text == "3.5");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3.5 * 2")).Text == "7");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("-3.5")).Text == "-3.5");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("(2.5 + 1.5) * (2.5 * (3.5 - 5.5))")).Text == "-20");
+                Console.WriteLine(new RPN().Calculate(parser.Parse("3 * -1")).Text == "-3");
+            }
+            catch (ParseError exc)
+            {
+                Console.WriteLine($"Unexpected symbol '{exc.Symbol}'");
+            }
+            catch (RPNError exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
 
             if (args.Length > 1)
             {
